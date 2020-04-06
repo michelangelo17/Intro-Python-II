1f99fbd9de27652b3a494332e656a2512c5107a9
michelangelo17 <michelangelo.markus@gmail.com>
2020-04-05 19:40:48 -0700

    day two MVP complete

--- src/player.py
+++ src/player.py
@@ -1,34 +1,69 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-directions = {
-    'n': {
-        'go': 'n_to',
-        'message': 'move North'
-    },
-    'e': {
-        'go': 'e_to',
-        'message': 'move East'
-    },
-    's': {
-        'go': 's_to',
-        'message': 'move South'
-    },
-    'w': {
-        'go': 'w_to',
-        'message': 'move West'
-    },
-}
-
 
 class Player:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, name, initial_room, items=None):
+        self.name = name
+        self.current_room = initial_room
+        if items == None:
+            self.items = []
+        else:
+            self.items = items
 
-    def move(self, d):
-        try:
-            self.location = getattr(self.location, directions[d]['go'])
-            print(directions[d]['message'].capitalize()+'!')
-        except:
-            print('You try to ' +
-                  directions[d]['message'] + ' but there\'s nowhere to go!')
+    def move(self, dir):
+        if getattr(self.current_room, f'{dir}_to'):
+            self.current_room = getattr(self.current_room, f'{dir}_to')
+            print('\nMove to:')
+            self.current_room.print_room()
+            self.current_room.print_items()
+        else:
+            print('\nNo path lies that direction.\n')
+
+    def take(self, item):
+        for i in self.current_room.items:
+            if i.name == item:
+                self.items.append(i)
+                self.current_room.items.remove(i)
+                i.on_take()
+                self.current_room.print_items()
+                break
+        else:
+            print(f'There is no {item} in the {self.current_room.name}\n')
+
+    def drop(self, item):
+        for i in self.items:
+            if i.name == item:
+                self.items.remove(i)
+                self.current_room.items.append(i)
+                i.on_drop()
+                self.current_room.print_items()
+                break
+        else:
+            print(f'You do not have {item} in your inventory.\n')
+
+    def inventory(self):
+        print('You are carrying:')
+        if len(self.items) > 0:
+            for i in self.items:
+                print(i.name)
+        else:
+            print('Nothing!')
+
+    def action(self, commands):
+        if len(commands) == 1:
+            if commands[0] in ('n', 'e', 's', 'w'):
+                self.move(commands[0])
+            elif commands[0] == 'i' or commands[0] == 'inventory':
+                self.inventory()
+            else:
+                raise ValueError('Unrecognised command!')
+        elif len(commands) == 2:
+            if commands[0] == 'take' or commands[0] == 'get':
+                self.take(commands[1])
+            elif commands[0] == 'drop':
+                self.drop(commands[1])
+            else:
+                raise ValueError('Unrecognised command!')
+        else:
+            raise IndexError('Unrecognised command!')
--- src/room.py
+++ src/room.py
@@ -1,8 +1,34 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+import textwrap
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=None):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+        if items == None:
+            self.items = []
+        else:
+            self.items = items
+
+    def print_room(self):
+        wrapper = textwrap.TextWrapper(width=80)
+        description = wrapper.wrap(text=self.description)
+        print(''.join((self.name, '\n\n')))
+        for s in description:
+            print(s)
+        print('\n')
+
+    def print_items(self):
+        if len(self.items) >= 1:
+            print(f'The {self.name.lower()} contains:')
+            for i in self.items:
+                print(i.name)
+            print('\n')
+        else:
+            print(f'The {self.name.lower()} is empty\n')
